04af19ddfff7b7b1c5e3a9755a1f4437d3cb51f0
Gianluca Bortoli <gianluca.bortoli@qlik.com>
2019-08-07 11:39:16 +0200

    Skip log lines with date in the past

--- pkg/monitor/monitor.go
+++ pkg/monitor/monitor.go
@@ -4,31 +4,37 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/cog-qlik/httpd-log-monitor/internal/logparser"
 	"github.com/cog-qlik/httpd-log-monitor/internal/tailer"
 	"github.com/hpcloud/tail"
 )
 
+// Monitor scrapes log files and derives statistics from it
 type Monitor struct {
-	parser   *logparser.HTTPd
-	tailer   *tailer.Tailer
-	log      *log.Logger
-	quitChan chan struct{}
+	parser    *logparser.HTTPd
+	tailer    *tailer.Tailer
+	log       *log.Logger
+	quitChan  chan struct{}
+	startTime time.Time
 }
 
+// New creates a monitor. Returns the monitor and an error
 func New(tailer *tailer.Tailer) (*Monitor, error) {
 	if tailer == nil {
 		return nil, fmt.Errorf("monitor needs a log tailer")
 	}
 	return &Monitor{
-		parser:   logparser.New(),
-		tailer:   tailer,
-		log:      log.New(os.Stderr, "", log.LstdFlags),
-		quitChan: make(chan struct{}),
+		parser:    logparser.New(),
+		tailer:    tailer,
+		log:       log.New(os.Stderr, "", log.LstdFlags),
+		quitChan:  make(chan struct{}),
+		startTime: time.Now(),
 	}, nil
 }
 
+// Start starts tailing and processing the log file in a separate goroutine
 func (m *Monitor) Start() error {
 	lines, err := m.tailer.Start()
 	if err != nil {
@@ -39,6 +45,7 @@ func (m *Monitor) Start() error {
 	return nil
 }
 
+// Stop stops the tailer and the processing of new log lines
 func (m *Monitor) Stop() error {
 	if err := m.tailer.Stop(); err != nil {
 		return err
@@ -47,15 +54,22 @@ func (m *Monitor) Stop() error {
 	return nil
 }
 
+// Wait blocks until the tailer goroutine is in a dead state.
+// Returns the reason for its death.
 func (m *Monitor) Wait() error {
 	return m.tailer.Wait()
 }
 
+// startParsingTail is the loop where every log line is parsed and processed
 func (m *Monitor) startParsingTail(lines <-chan *tail.Line) {
 	for {
 		select {
 		case l := <-lines:
-			m.handleLine(l)
+			good, err := m.filterLine(l)
+			if err != nil {
+				m.log.Println("[ERROR]", err)
+			}
+			m.log.Println(good)
 		case <-m.quitChan:
 			m.log.Println("[INFO] exiting monitor")
 			return
@@ -63,13 +77,29 @@ func (m *Monitor) startParsingTail(lines <-chan *tail.Line) {
 	}
 }
 
-func (m *Monitor) handleLine(line *tail.Line) {
+func (m *Monitor) filterLine(line *tail.Line) (*logparser.Line, error) {
 	if line == nil {
-		return
+		return nil, fmt.Errorf("nil line")
 	}
 	parsedLine, err := m.parser.ParseLine(line.Text)
 	if err != nil {
-		m.log.Printf("[ERROR] cannot parse line: %v\n", err)
+		return nil, fmt.Errorf("error parsing line: %v", err)
 	}
-	m.log.Printf("%v", parsedLine)
+	// Skip log lines whose date is before the start of the monitor.
+	// This avoids to consider stale data for any later usage (eg. stats)
+	if m.isOldLine(parsedLine) {
+		return nil, fmt.Errorf("old log line detected. log time: %s, monitor start time: %s",
+			parsedLine.Date.String(), m.startTime.String())
+	}
+	return parsedLine, nil
+}
+
+func (m *Monitor) isOldLine(line *logparser.Line) bool {
+	if line == nil {
+		return true
+	}
+	if line.Date.Before(m.startTime) {
+		return true
+	}
+	return false
 }
--- pkg/monitor/monitor_test.go
+++ pkg/monitor/monitor_test.go
@@ -3,9 +3,12 @@ package monitor
 import (
 	"os"
 	"testing"
+	"time"
 
+	"github.com/cog-qlik/httpd-log-monitor/internal/logparser"
 	"github.com/cog-qlik/httpd-log-monitor/internal/tailer"
 	"github.com/cog-qlik/httpd-log-monitor/internal/testutils"
+	"github.com/hpcloud/tail"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -91,4 +94,93 @@ func TestMonitor_WaitWhenNotStarted(t *testing.T) {
 
 	err := m.Wait()
 	assert.Error(t, err)
+}
+
+func TestMonitor_FilterLine(t *testing.T) {
+	layout := "02/Jan/2006:15:04:05 -0700"
+	pastDate := "09/May/2018:16:00:39 +0000"
+	futureDate := "09/May/2099:16:00:39 +0000"
+	futureDateTime, _ := time.Parse(layout, futureDate)
+
+	testCases := []struct {
+		line          *tail.Line
+		expParsedLine *logparser.Line
+		expErr        bool
+	}{
+		{
+			nil,
+			nil,
+			true,
+		},
+		{
+			&tail.Line{},
+			nil,
+			true,
+		},
+		{
+			// Date in the past
+			&tail.Line{
+				Text: `127.0.0.1 asd james [` + pastDate + `] "GET /report HTTP/1.0" 200 123`,
+				Err:  nil,
+				Time: time.Now(),
+			},
+			nil,
+			true,
+		},
+		{
+			// Date in the future
+			&tail.Line{
+				Text: `127.0.0.1 asd james [` + futureDate + `] "GET /report HTTP/1.0" 200 123`,
+				Err:  nil,
+				Time: time.Now(),
+			},
+			&logparser.Line{
+				RemoteHost:    "127.0.0.1",
+				RemoteLogName: "asd",
+				User:          "james",
+				Date:          futureDateTime,
+				Method:        "GET",
+				Path:          "/report",
+				Protocol:      "HTTP/1.0",
+				StatusCode:    200,
+				ContentLength: 123,
+			},
+			false,
+		},
+	}
+
+	m, _ := getTestMonitor()
+
+	for _, tt := range testCases {
+		parsed, err := m.filterLine(tt.line)
+		assert.Equal(t, tt.expErr, err != nil)
+		assert.Equal(t, tt.expParsedLine, parsed)
+	}
+}
+
+func TestMonitor_IsOldLine(t *testing.T) {
+	testCases := []struct {
+		line     *logparser.Line
+		expIsOld bool
+	}{
+		{
+			&logparser.Line{Date: time.Now().AddDate(0, 0, -1)},
+			true,
+		},
+		{
+			&logparser.Line{Date: time.Now().AddDate(0, 0, +1)},
+			false,
+		},
+		{
+			nil,
+			true,
+		},
+	}
+
+	m, _ := getTestMonitor()
+
+	for _, tt := range testCases {
+		isOld := m.isOldLine(tt.line)
+		assert.Equal(t, tt.expIsOld, isOld)
+	}
 }
